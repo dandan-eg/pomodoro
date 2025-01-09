3c3bdbea8a953dbc5aa5c0bfd90476e5b615cb8b
dandan-eg <egalledev@gmail.com>
2025-01-09 20:47:05 +0100

    feat: use binary id for the primary of Timers

--- priv/repo/migrations/20250109185219_create_timers.exs
+++ priv/repo/migrations/20250109185219_create_timers.exs
@@ -2,7 +2,8 @@ defmodule Pomodoro.Repo.Migrations.CreateTimers do
   use Ecto.Migration
 
   def change do
-    create table(:timers) do
+    create table(:timers, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :start, :integer
       add :duration, :integer
 
