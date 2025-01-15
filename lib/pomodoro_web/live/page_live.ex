82484c4be21941665e8d9e5d7dd3d5ce3947e70e
dandan-eg <egalledev@gmail.com>
2025-01-15 16:30:39 +0100

    clean up live view

--- lib/pomodoro_web/live/page_live.ex
+++ lib/pomodoro_web/live/page_live.ex
@@ -3,26 +3,27 @@ defmodule PomodoroWeb.PageLive do
 
   alias Pomodoro.Timers
 
+  @impl true
   def mount(_params, _session, socket) when socket.assigns.live_action == :create do
     {:ok, timer} = Timers.create_timer()
     {:ok, redirect(socket, to: ~p"/#{timer}")}
   end
 
+  @impl true
   def mount(%{"timer_id" => timer_id}, _session, socket)
       when socket.assigns.live_action == :show do
-    {:ok, get_timer_or_redirect(socket, timer_id)}
+    timer = Timers.get_timer!(timer_id)
+
+    {:ok, assign(socket, timer: timer)}
+  rescue
+    _ ->
+      redirect(socket, to: ~p"/")
   end
 
+  @impl true
   def render(assigns) do
     ~H"""
     <p id="timer" phx-hook="Timer" data-start-time={@timer.start} data-duration={@timer.duration}></p>
     """
-  end
-
-  def get_timer_or_redirect(socket, timer_id) do
-    assign(socket, timer: Timers.get_timer!(timer_id))
-  catch
-    Ecto.NoResultsError ->
-      redirect(socket, to: ~p"/")
   end
 end
